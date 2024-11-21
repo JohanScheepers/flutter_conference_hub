4f691aa72ef0d02256055bf9ccacbbf245f546d0
JohanScheepersf <scheepersjohanf@gmail.com>
2024-11-21 22:16:49 +0200

    Update

--- lib/app/pages/attendee/badge_attendee.dart
+++ lib/app/pages/attendee/badge_attendee.dart
@@ -1,3 +1,5 @@
+//Information: Name, Photo, Title, Company, Contact Details, QR Code (for scanning), Conference Pass (e.g., VIP, Speaker, etc.)\nCustomization: Ability to change the background color or add a personal touch.\nAccessibility: Ensure the badge is accessible for users with visual impairments (e.g., using larger font sizes or alternative color schemes).
+
 import 'package:flutter/material.dart';
 
 class BadgeAttendeePage extends StatefulWidget {
@@ -10,12 +12,21 @@ class BadgeAttendeePage extends StatefulWidget {
 class _BadgeAttendeePageState extends State<BadgeAttendeePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Badge"),
+    return const Scaffold(
+      body: Column(
+        children: [
+          //User Prfile
+          Stack(children: [
+            Text("Header Pic"),
+            Text("Profile Pic"),
+            Text("Edit Profile")
+          ]),
+          Text("UserName"),
+          Text("Social media handles"),
+          Text("QR code"),
+          //Schedule for what talks attendee wants to attend
+        ],
       ),
-      body: const Text(
-          "Information: Name, Photo, Title, Company, Contact Details, QR Code (for scanning), Conference Pass (e.g., VIP, Speaker, etc.)\nCustomization: Ability to change the background color or add a personal touch.\nAccessibility: Ensure the badge is accessible for users with visual impairments (e.g., using larger font sizes or alternative color schemes)."),
     );
   }
 }
