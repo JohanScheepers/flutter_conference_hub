b1e44f3a6cc8835e86bf0c09f2160a87493e84f9
JohanScheepersf <scheepersjohanf@gmail.com>
2025-09-17 11:16:43 +0200

    Update sponsors

--- lib/dummy_data/dummy_data_sponsors.dart
+++ lib/dummy_data/dummy_data_sponsors.dart
@@ -2,6 +2,6 @@ import 'package:flutter_conference_hub/app/models/models.dart';
 
 List<Sponsors> sponsors = [
   Sponsors(
-      "Add your support", "to the project", "as this is for your community"),
+      "Add your support", "to the project", "as this is your community"),
   Sponsors("We need", "designers and dev", "to showcase their skills")
 ];
